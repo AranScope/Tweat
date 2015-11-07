2d6e3b7b9ec2d022ff03a7c27fe4525074ae4b1e
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-11-07 17:40:33 +0000

    Things

--- src/TwitterW.java
+++ src/TwitterW.java
@@ -11,25 +11,26 @@ import twitter4j.conf.ConfigurationBuilder;
 
 import javax.imageio.ImageIO;
 import javax.xml.ws.Response;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class Twitter {
+public class TwitterW {
 
     private static twitter4j.Twitter wrapper = TwitterFactory.getSingleton();
-    private static TwitterStream stream = new TwitterStreamFactory(new ConfigurationBuilder().setJSONStoreEnabled(true).build());
+    private static TwitterStream stream = new TwitterStreamFactory(new ConfigurationBuilder().setJSONStoreEnabled(true).build()).getInstance();
     private static ArrayList<Long> listenedUsers = new ArrayList<>();
     private static FilterQuery listenQuery = new FilterQuery();
 
     static {
-        stream.setOAuthAccessToken(wrapper.getOAuthAccessToken());
-        stream.setOAuthConsumer("xqlljOXRePfUjb5D48sXrUPUo", "zzq4TpUANIwQTXIiw5Sbio1OHuzAEHUbo4bJMx4RKgoemPTtNC");
+        try {
+            stream.setOAuthAccessToken(wrapper.getOAuthAccessToken());
+            stream.setOAuthConsumer("xqlljOXRePfUjb5D48sXrUPUo", "zzq4TpUANIwQTXIiw5Sbio1OHuzAEHUbo4bJMx4RKgoemPTtNC");
+        } catch (TwitterException e){
+            System.err.println("TwitterW.<static> : " + e.getErrorMessage());
+        }
     }
-
     /**
      * Add a status listener, which is fired whenever a listened user makes a tweet
      * @param listener
@@ -42,21 +43,26 @@ public class Twitter {
      * Tweet from the game's account
      * @param msg
      */
-    public static void tweet(String msg) {
+    public static void tweet(String msg) throws TwitterException{
         StatusUpdate status = new StatusUpdate(msg);
         wrapper.updateStatus(status);
         System.out.println(status);
     }
 
+    public static void refreshFollowers() {
+        wrapper.getFollowersIDs()
+    }
+
     /**
-     * Listens to tweets from the specified user and executes the listener
+     * Listens to tweets from the specified user.
+     * <br>
+     * The listeners that have been added by onTweet() will be triggered when the user tweets
      * @param user
-     * @param listener
      */
     public static void listen(User user) {
         listenedUsers.add(user.getId());
-        query.follow(listenedUsers.toArray(new double [listenedUsers.size()]));
-        stream.filter(query);
+        for(long l : listenedUsers) listenQuery.follow(l);
+        stream.filter(listenQuery);
     }
 
     /**
@@ -86,7 +92,7 @@ public class Twitter {
      * @return
      */
     public static double getScore(User user) {
-        double d = getFollowers(user) + ((double)getLikes(user) / (getFollowing(user) + 1)));
+        double d = getFollowers(user) + ((double)getLikes(user) / (getFollowing(user) + 1));
         return d <= 0 ? 1 : Math.log(d);
     }
 
@@ -95,7 +101,7 @@ public class Twitter {
      * @param handle
      * @return
      */
-    public static User getUser(String handle) {
+    public static User getUser(String handle) throws TwitterException{
         ResponseList<User> response = wrapper.lookupUsers(handle);
         return response.size() == 0 ? null : response.get(0);
     }
@@ -135,16 +141,17 @@ public class Twitter {
      * @return
      */
     public static BufferedImage getProfileImage(User user) {
+        File dest;
         try {
-            File dest = File.createTempFile("res/tmp/" + user.getName() + ".png");
+            dest = File.createTempFile("res/tmp/" + user.getName(), ".png");
             URL url = new URL(getProfileImageURL(user));
             InputStream is = url.openStream();
             OutputStream os = new FileOutputStream(dest);
 
             byte[] bytes = new byte[1024];
-            int length = 0;
+            int length;
 
-            while ((length = is.read(readBytes)) != -1) {
+            while ((length = is.read(bytes)) != -1) {
                 os.write(bytes, 0, length);
             }
 
@@ -152,7 +159,8 @@ public class Twitter {
             os.close();
             return ImageIO.read(dest);
         } catch (IOException e) {
-            System.err.println("Twitter.getProfileImage() : Error loading temp file (" + dest.getAbsolutePath() + ") : " + e.getMessage());
+            System.err.println("Twitter.getProfileImage() : Error loading temp file : " + e.getMessage());
+            return null;
         }
     }
 
