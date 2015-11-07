5eb4c03f82da479d32063b7be210d487a8bb2959
aranscope <aranlong@live.co.uk>
2015-11-07 18:22:27 +0000

    twitter libraries updates

---
+++ src/AranTestClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by aranscope on 07/11/15.
+ */
+public class AranTestClass {
+    public static void main(String[] args){
+        try {
+            TwitterW.tweet("@AranScope I think it's working...");
+        }catch(Exception e){}
+    }
+}
